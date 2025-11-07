2daf34fef6f228bcb96efb3c9f8a2cdd2b33cee0
Kyle Mann <kylemann90@gmail.com>
2025-11-07 01:10:52 -0700

    Fix Vite bundling for vanilla JS project

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  root: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
   server: {
     port: 5173,
     open: true
